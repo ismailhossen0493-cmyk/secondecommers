ccba98b92cfcca255f321de3be620a74ad3f4281
ismailhossen0493-cmyk <ismailhossen0493@gmail.com>
2026-02-26 17:28:12 +0600

    Update products.js

--- kin-store-complete/ecommerce/backend/routes/products.js
+++ kin-store-complete/ecommerce/backend/routes/products.js
@@ -1,16 +1,14 @@
-// backend/routes/products.js
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/productController');
 const { protect } = require('../middleware/auth');
-const { adminOnly } = require('../middleware/roles');
 
 router.get('/', ctrl.getAllProducts);
-router.get('/admin/all', protect, adminOnly, ctrl.adminGetAllProducts);
+router.get('/admin/all', protect, ctrl.adminGetAllProducts);
 router.get('/:id', ctrl.getProduct);
 
-router.post('/', protect, adminOnly, ctrl.createProduct);
-router.patch('/:id', protect, adminOnly, ctrl.updateProduct);
-router.delete('/:id', protect, adminOnly, ctrl.deleteProduct);
+router.post('/', protect, ctrl.createProduct);
+router.patch('/:id', protect, ctrl.updateProduct);
+router.delete('/:id', protect, ctrl.deleteProduct);
 
 module.exports = router;
